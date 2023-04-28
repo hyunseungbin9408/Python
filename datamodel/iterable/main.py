4a0510fa08bfb9eeab1f715e1b24751cd777b7f8
hyunseungbin <hyunseungbin9408@gmail.com>
2023-04-28 11:35:55 +0900

    python_study_day2

---
+++ datamodel/iterable/main.py
@@ -0,0 +1,5 @@
+#number = 100
+number = [100,200]
+for num in number:
+    print(num)
+
--- tuple/nametuple.py
+++ tuple/nametuple.py
@@ -1,2 +1,8 @@
-from collections import nametuple
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+seoul = City('Seoul','KR', 36.933, (35.689722, 139.691667))
+tokyo = City('tokyu','JP', 36.933, (35.689722, 139.691667))
+make_city = City._make(tokyo)
+print(make_city.__add__(City._make(seoul)))
 
